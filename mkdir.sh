98a3dabf5131a8a20bfdad669338b7d0232bf8d3
Your Name <you@example.com>
2020-04-26 10:44:41 +0000

    awdaw

---
+++ mkdir.sh
@@ -0,0 +1,6 @@
+mkdir prediction/resources/raw
+mkdir prediction/resources/train
+mkdir prediction/resources/test
+mkdir prediction/resources/vad
+mkdir prediction/resources/models
+mkdir prediction/resources/tensorboards
